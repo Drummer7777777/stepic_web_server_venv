7aad0ad0d38c1c340ab16d173b4c5be147731d83
Drummer7777777 <73654451+Drummer7777777@users.noreply.github.com>
2022-04-22 00:38:00 +0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 sudo unlink /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/stepic_web_server/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 #sudo ln -s /home/box/stepic_web_server/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start
-sudo gunicorn --bind 0.0.0.0:8080 hello:wsgi_application
-sudo gunicorn --bind 0.0.0.0:8000 ask.wsgi:application
+#sudo gunicorn --bind 0.0.0.0:8080 hello:wsgi_application
+#sudo gunicorn --bind 0.0.0.0:8000 ask.wsgi:application
