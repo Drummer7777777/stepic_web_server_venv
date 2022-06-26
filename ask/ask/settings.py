a321076fff95abd7e24869842346d9eb7386e155
Drummer7777777 <73654451+Drummer7777777@users.noreply.github.com>
2022-06-26 16:37:24 +0500

    add ALLOWED_HOSTS = ['*']

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0ppmqhkvw^-5ag_gwbm1q542(0a7@q6&p0t85ff%c#!$t4k91h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
